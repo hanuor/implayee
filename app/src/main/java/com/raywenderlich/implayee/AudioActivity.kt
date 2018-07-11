b4a36d9c143116e5bca1ef1d29a531afdc1a90b3
Shantanu <shanjohri@gmail.com>
2018-07-11 16:38:59 +0530

    Fixes and changes

--- app/src/main/java/com/raywenderlich/implayee/AudioActivity.kt
+++ app/src/main/java/com/raywenderlich/implayee/AudioActivity.kt
@@ -4,54 +4,82 @@ import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 
 class AudioActivity : AppCompatActivity() {
     private var playPauseAudio: Button? = null
     private var restartAudio: Button? = null
-    private val mediaPlayer = MediaPlayer()
+    private var mediaPlayer: MediaPlayer? = null
+    private var stoppageTime = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio)
-        //Setting up the mediaplayer to stream Music
+
+        mediaPlayer = MediaPlayer()
+        //Setting up the mediaPlayer? to stream Music
         val audioDataSource =
                 "android.resource://com.raywenderlich.implayee/" + R.raw.sample_audio
-        try {
-            mediaPlayer.setDataSource(this, Uri.parse(audioDataSource))
-            mediaPlayer.prepare()
-            mediaPlayer.start()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+        mediaPlayer?.setDataSource(this, Uri.parse(audioDataSource))
+        mediaPlayer?.prepare()
+
         initViews()
         onClicks()
 
     }
 
     private fun initViews() {
-        //play / pause audio button (mediaplayer class)
+        //play / pause audio button (mediaPlayer? class)
         playPauseAudio = findViewById(R.id.playPauseAudio)
 
         //restart Audio button
         restartAudio = findViewById(R.id.restartAudio)
+        playPauseAudio?.text = this.getString(R.string.audio_play)
+
     }
 
     private fun onClicks() {
         //on click operations
-        playPauseAudio?.setOnClickListener({
-            if (mediaPlayer.isPlaying) {
-                mediaPlayer.pause()
+        playPauseAudio?.setOnClickListener {
+
+            if (mediaPlayer?.isPlaying as Boolean) {
+                mediaPlayer?.pause()
                 playPauseAudio?.text = this.getString(R.string.audio_play)
             } else {
-                mediaPlayer.start()
+                mediaPlayer?.start()
                 playPauseAudio?.text = this.getString(R.string.audio_pause)
+
+                mediaPlayer?.setOnCompletionListener {
+                    playPauseAudio?.text = this.getString(R.string.audio_play)
+                }
             }
 
-        })
-        restartAudio?.setOnClickListener({
-            mediaPlayer.seekTo(0)
-            mediaPlayer.start()
+
+        }
+        restartAudio?.setOnClickListener {
+            mediaPlayer?.seekTo(0)
+            mediaPlayer?.start()
             playPauseAudio?.text = this.getString(R.string.audio_pause)
-        })
+        }
     }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer?.pause()
+        stoppageTime = mediaPlayer?.currentPosition as Int
+        playPauseAudio?.text = this.getString(R.string.audio_play)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (stoppageTime > 0) {
+            //seeking to the time when onPause() was called
+            mediaPlayer?.seekTo(stoppageTime)
+            mediaPlayer?.start()
+            playPauseAudio?.text = this.getString(R.string.audio_pause)
+        }
+    }
+
+
 }
